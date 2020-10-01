08fa4dfb4f46222e9c249e545d9c4679cfe5d2d9
Kartik Bhushan <kartik@xecurify.com>
2020-10-01 13:32:12 +0530

    Updates

---
+++ precache-manifest.8c448502339ab54be95f9c5fbc75ceac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beea9ca391d197a5ee92ea2396059e65",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "15e9f05181bace099e98",
+    "url": "/React-Weather-App/static/css/main.e99f88f2.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/React-Weather-App/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e9f05181bace099e98",
+    "url": "/React-Weather-App/static/js/main.6ed1b722.chunk.js"
+  },
+  {
+    "revision": "080e3634d012f4a64070",
+    "url": "/React-Weather-App/static/js/runtime-main.ac207c33.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/React-Weather-App/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/React-Weather-App/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
